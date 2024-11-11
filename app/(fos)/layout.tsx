132fb98b4b602eb11188439ac2b3fbd83aa44ac0
izi Achat <iziachat@gmail.com>
2024-11-11 17:03:29 -0500

    add auth

--- app/(fos)/layout.tsx
+++ app/(fos)/layout.tsx
@@ -1,11 +1,13 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
 const LayoutPage = async ({ children }: PropsWithChildren) => {
-  // const user = await currentUser();
+  const user = await currentUser();
 
-  // if (!user) {
-  //   redirect('/sign-in');
-  // }
+  if (!user) {
+    redirect('/sign-in');
+  }
   return <div>{children}</div>;
 };
 
